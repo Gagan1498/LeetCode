4535f79310ba1f61362c268239f12583756b3054
Gagan B <46404410+Gagan1498@users.noreply.github.com>
2024-03-27 01:22:51 -0700

    Update 645: Set Mismatch.c

--- 645: Set Mismatch.c
+++ 645: Set Mismatch.c
@@ -1,6 +1,7 @@
 /* Trick: In such questions where set is given in the form of [1,n]..they can be used to self index. For example, to find the duplicate 
-number I can somehow point the duplicate number to the same (previous identified number. And to find the lost number - trick is the "to
-go to any index, you should have (i+1) number, where i is the index */
+number I can somehow point the duplicate number to the same (previous identified number), this will allow me to catch the duplicated number. 
+And to find the lost number - trick is "to go to any index, you should have (i+1) number, where i is the index....why? Because I was never 
+able to reach that index "nums[abs(nums[i])-1]". More exaplanation at: https://www.youtube.com/watch?v=j89Yzq3IwVY&t=918s&ab_channel=codestorywithMIK */
 
 /**
  * Note: The returned array must be malloced, assume caller calls free().
