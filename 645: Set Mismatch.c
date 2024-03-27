d1a950979047d0fb0ad5783f1c80cf5c6a441206
Gagan B <46404410+Gagan1498@users.noreply.github.com>
2024-03-27 01:19:15 -0700

    Update 645: Set Mismatch.c

--- 645: Set Mismatch.c
+++ 645: Set Mismatch.c
@@ -1,3 +1,7 @@
+/* Trick: In such questions where set is given in the form of [1,n]..they can be used to self index. For example, to find the duplicate 
+number I can somehow point the duplicate number to the same (previous identified number. And to find the lost number - trick is the "to
+go to any index, you should have (i+1) number, where i is the index */
+
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
