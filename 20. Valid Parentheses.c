764ce9a955f7648c03dd4788746c277a57676908
Gagan B. <46404410+Gagan1498@users.noreply.github.com>
2024-12-28 23:13:57 -0800

    Update 20. Valid Parentheses.c

--- 20. Valid Parentheses.c
+++ 20. Valid Parentheses.c
@@ -2,41 +2,34 @@
 
 bool isValid(char* s) {
 
-    int size = strlen(s);
-    char stack_arr[size + 1];
+    long int size = strlen(s);
+    char arr[size+1];
     int top = -1;
 
-    if (size==1) return false;
-    for (int i = 0; i < size; i++) {
-        if (*(s + i) == '(' || *(s + i) == '{' || *(s + i) == '[') {
+    for(int i=0;i<size;i++){
+        if (*(s+i) == '(' || *(s+i) == '[' || *(s+i) == '{'){
             top++;
-            stack_arr[top] = *(s + i);
+            arr[top] = *(s+i);
+            printf("1 %c\n",*(s+i));
         }
 
-        else if (*(s + i) == ')' || *(s + i) == '}' || *(s + i) == ']') {
-            if(top != -1){
-                if (*(s + i) == ')' && stack_arr[top] == '(') {
-                    top--;
-                }
-                else if (*(s + i) == '}' && stack_arr[top] == '{') {
-                    top--;
-                }
-                else if (*(s + i) == ']' && stack_arr[top] == '[')
-                 {
-                    top--;
-                }
-                else return false;
-            }
-            else {
-                return false;
-            }
+        else if 
+            (top != -1 &&
+            ((*(s+i) == ')' && arr[top] == '(') || 
+            (*(s+i) == ']' && arr[top] == '[') || 
+            (*(s+i) == '}' && arr[top] == '{'))){
+            printf("2 %c\n",*(s+i));
+            top--;
+            printf("3 %c\n",*(s+i));
         }
 
-        else {
+        else{
+            printf("4 %c\n",*(s+i));
             return false;
-        }
+        }        
+
     }
 
-    if(top!=-1) return false;
+    if(top != -1) return false;
     return true;
 }
